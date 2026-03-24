d33ff15abc4eaf40ab2f13aa85051c1a94d8061b
AnjanaKamle <kamleanjana@gmail.com>
2026-03-24 10:00:00 +0530

    clickable

--- pages/script.js
+++ pages/script.js
@@ -1,149 +1,186 @@
-// elements
-var container = document.getElementById("countriesContainer");
-var searchInput = document.getElementById("searchInput");
-var regionFilter = document.getElementById("regionFilter");
-var sortOption = document.getElementById("sortOption");
-var loading = document.getElementById("loading");
-var error = document.getElementById("error");
-var toggleBtn = document.getElementById("themeToggle");
+window.onload = function () {
 
-var countriesData = [];
+  var container = document.getElementById("countriesContainer");
+  var searchInput = document.getElementById("searchInput");
+  var regionFilter = document.getElementById("regionFilter");
+  var sortOption = document.getElementById("sortOption");
+  var loading = document.getElementById("loading");
+  var error = document.getElementById("error");
+  var toggleBtn = document.getElementById("themeToggle");
 
-/* theme toggle */
-toggleBtn.onclick = function () {
-  document.body.classList.toggle("dark");
-};
+  var popup = document.getElementById("popup");
+  var popupData = document.getElementById("popupData");
+  var closePopup = document.getElementById("closePopup");
 
-/* fetch data */
-function fetchCountries() {
-  loading.style.display = "block";
-  error.style.display = "none";
+  var countriesData = [];
 
-  fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags,cca2")
-    .then(function (res) {
-      return res.json();
-    })
-    .then(function (data) {
-      countriesData = data;
-      displayCountries(countriesData);
-      loading.style.display = "none";
-    })
-    .catch(function () {
-      loading.style.display = "none";
-      error.style.display = "block";
-      error.innerText = "Failed to load data";
+  // THEME
+  if (toggleBtn) {
+    toggleBtn.onclick = function () {
+      document.body.classList.toggle("dark");
+    };
+  }
+
+  // CLOSE POPUP (SAFE)
+  if (closePopup) {
+    closePopup.onclick = function () {
+      popup.classList.add("hidden");
+    };
+  }
+
+  // FETCH
+  function fetchCountries() {
+    loading.style.display = "block";
+    error.style.display = "none";
+
+    fetch("https://restcountries.com/v3.1/all?fields=name,capital,region,population,flags,cca2")
+      .then(function (res) {
+        return res.json();
+      })
+      .then(function (data) {
+        countriesData = data;
+        displayCountries(countriesData);
+        loading.style.display = "none";
+      })
+      .catch(function () {
+        loading.style.display = "none";
+        error.style.display = "block";
+        error.innerText = "Failed to load data";
+      });
+  }
+
+  // DISPLAY
+  function displayCountries(data) {
+    container.innerHTML = "";
+
+    for (var i = 0; i < data.length; i++) {
+      var country = data[i];
+
+      if (!country.name || !country.name.common) continue;
+
+      var div = document.createElement("div");
+      div.className = "country-card";
+
+      div.onclick = (function (c) {
+        return function () {
+          showPopup(c);
+        };
+      })(country);
+
+      var img = document.createElement("img");
+      img.className = "flag";
+
+      var code = country.cca2;
+      img.src = code
+        ? "https://flagcdn.com/w320/" + code.toLowerCase() + ".png"
+        : "https://flagcdn.com/w320/un.png";
+
+      var name = document.createElement("h3");
+      name.innerText = country.name.common;
+
+      var capital = document.createElement("p");
+      capital.innerText =
+        "Capital: " +
+        (country.capital && country.capital[0] ? country.capital[0] : "N/A");
+
+      // var region = document.createElement("p");
+      // region.innerText = "Region: " + (country.region || "N/A");
+
+      // var pop = document.createElement("p");
+      // pop.innerText =
+      //   "Population: " +
+      //   (country.population ? country.population.toLocaleString() : "N/A");
+
+      div.appendChild(img);
+      div.appendChild(name);
+      div.appendChild(capital);
+      // div.appendChild(region);
+      // div.appendChild(pop);
+
+      container.appendChild(div);
+    }
+  }
+
+  // FILTER
+  function applyFilters() {
+    var filtered = countriesData;
+
+    var text = searchInput.value.toLowerCase();
+
+    filtered = filtered.filter(function (c) {
+      var name = c.name && c.name.common ? c.name.common.toLowerCase() : "";
+      var capital = c.capital && c.capital[0] ? c.capital[0].toLowerCase() : "";
+
+      return name.includes(text) || capital.includes(text);
     });
-}
 
-/* countries display */
-function displayCountries(data) {
-  container.innerHTML = "";
-
-  for (var i = 0; i < data.length; i++) {
-    var country = data[i];
-
-    // skip broken data
-    if (!country.name || !country.name.common) continue;
-
-    var div = document.createElement("div");
-    div.className = "country-card";
-
-    // FLAG
-    var img = document.createElement("img");
-    img.className = "flag";
-
-    var code = country.cca2;
-    if (code) {
-      img.src = "https://flagcdn.com/w320/" + code.toLowerCase() + ".png";
-    } else {
-      img.src = "https://flagcdn.com/w320/un.png";
+    var region = regionFilter.value;
+    if (region !== "") {
+      filtered = filtered.filter(function (c) {
+        return c.region === region;
+      });
     }
 
-    // name
-    var name = document.createElement("h3");
+    if (sortOption.value === "name-asc") {
+      filtered.sort(function (a, b) {
+        return a.name.common.localeCompare(b.name.common);
+      });
+    }
+
+    if (sortOption.value === "name-desc") {
+      filtered.sort(function (a, b) {
+        return b.name.common.localeCompare(a.name.common);
+      });
+    }
+
+    if (sortOption.value === "pop-asc") {
+      filtered.sort(function (a, b) {
+        return (a.population || 0) - (b.population || 0);
+      });
+    }
+
+    if (sortOption.value === "pop-desc") {
+      filtered.sort(function (a, b) {
+        return (b.population || 0) - (a.population || 0);
+      });
+    }
+
+    displayCountries(filtered);
+  }
+
+  // POPUP
+  function showPopup(country) {
+    popup.classList.remove("hidden");
+
+    popupData.innerHTML = "";
+
+    var name = document.createElement("h2");
     name.innerText = country.name.common;
 
-    // capital
     var capital = document.createElement("p");
     capital.innerText =
       "Capital: " +
       (country.capital && country.capital[0] ? country.capital[0] : "N/A");
 
-    // region
     var region = document.createElement("p");
     region.innerText = "Region: " + (country.region || "N/A");
 
-    // population
-    var pop = document.createElement("p");
-    pop.innerText = "Population: " + (country.population || "N/A");
+    var population = document.createElement("p");
+    population.innerText =
+      "Population: " +
+      (country.population ? country.population.toLocaleString() : "N/A");
 
-    // append
-    div.appendChild(img);
-    div.appendChild(name);
-    div.appendChild(capital);
-    div.appendChild(region);
-    div.appendChild(pop);
-
-    container.appendChild(div);
-  }
-}
-
-/* filter, search, sort */
-function applyFilters() {
-  var filtered = countriesData;
-
-  var text = searchInput.value.toLowerCase();
-
-  // SEARCH
-  filtered = filtered.filter(function (c) {
-    var name =
-      c.name && c.name.common ? c.name.common.toLowerCase() : "";
-    var capital =
-      c.capital && c.capital[0] ? c.capital[0].toLowerCase() : "";
-
-    return name.includes(text) || capital.includes(text);
-  });
-
-  // region
-  var region = regionFilter.value;
-  if (region !== "") {
-    filtered = filtered.filter(function (c) {
-      return c.region === region;
-    });
+    popupData.appendChild(name);
+    popupData.appendChild(capital);
+    popupData.appendChild(region);
+    popupData.appendChild(population);
   }
 
-  // sorting
-  if (sortOption.value === "name-asc") {
-    filtered.sort(function (a, b) {
-      return a.name.common.localeCompare(b.name.common);
-    });
-  }
+  // EVENTS
+  searchInput.oninput = applyFilters;
+  regionFilter.onchange = applyFilters;
+  sortOption.onchange = applyFilters;
 
-  if (sortOption.value === "name-desc") {
-    filtered.sort(function (a, b) {
-      return b.name.common.localeCompare(a.name.common);
-    });
-  }
-
-  if (sortOption.value === "pop-asc") {
-    filtered.sort(function (a, b) {
-      return (a.population || 0) - (b.population || 0);
-    });
-  }
-
-  if (sortOption.value === "pop-desc") {
-    filtered.sort(function (a, b) {
-      return (b.population || 0) - (a.population || 0);
-    });
-  }
-
-  displayCountries(filtered);
-}
-
-/* events */
-searchInput.oninput = applyFilters;
-regionFilter.onchange = applyFilters;
-sortOption.onchange = applyFilters;
-
-/* start */
-fetchCountries();
+  // START
+  fetchCountries();
+};
